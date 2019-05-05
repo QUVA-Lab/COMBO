f4713cb2142ef010d75129db5b41123b8e47d4ac
ChangYong-Oh <changyong.oh0224@gmail.com>
2019-05-05 22:53:23 +0200

    ising 3 lambdas das5 batch command (sequentially)

--- run_combo_ising.sh
+++ run_combo_ising.sh
@@ -2,6 +2,6 @@
 
 
 
-srun python GraphDecompositionBO/main.py --objective ising --lamda 0.01   --n_eval 150 --no_graph_learning --random_seed_config $1
-srun python GraphDecompositionBO/main.py --objective ising --lamda 0.0001 --n_eval 150 --no_graph_learning --random_seed_config $1
-srun python GraphDecompositionBO/main.py --objective ising --lamda 0.0    --n_eval 150 --no_graph_learning --random_seed_config $1
+srun python GraphDecompositionBO/main.py --parallel --objective ising --lamda 0.01   --n_eval 150 --no_graph_learning --random_seed_config $1
+srun python GraphDecompositionBO/main.py --parallel --objective ising --lamda 0.0001 --n_eval 150 --no_graph_learning --random_seed_config $1
+srun python GraphDecompositionBO/main.py --parallel --objective ising --lamda 0.0    --n_eval 150 --no_graph_learning --random_seed_config $1
